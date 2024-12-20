a7523f994ff18cc95ffb341ef7249f43c213a9b5
unknown <vipboyxu2k3@gmail.com>
2024-12-20 16:39:19 +0700

    added cart visibility

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import Header from './components/Layout/Header';
-import Meals from './components/Meals/Meals';
-import CartProvider from './store/CartProvider';
+import { useState } from "react";
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import CartProvider from "./store/CartProvider";
 
 function App() {
+  const [cart, setCart] = useState(false);
+
+  const showCartHandler = () => {
+    setCart(true);
+  };
+
+  const hideCartHandler = () => {
+    setCart(false);
+  };
+
   return (
     <CartProvider>
-      <Header />
+      {cart && <div>Cart...</div>}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
